e36e1559d20b4ddd66298e6bc5f86f3630163698
hm34400 <hichamelyaagoubi34@gmail.com>
2017-01-06 10:23:58 +0100

    rectification du timestamp

--- Evaluation.py
+++ Evaluation.py
@@ -5,8 +5,8 @@ tablTheme = []
 tablQuest = []
 tablQcm = []
 tablRep = []
+tablStamp = []
 line = ""
-evaluation = False
 
 # Fonction qui lit le fichier questions.qs et trie les lignes
 def fetch():
@@ -49,39 +49,61 @@ def eval():
             else :
                 # on demande la reponse a la question indiquee
                 rep= raw_input(tablQuest[i])
+                # on stocke le timestamp dans le tableau Stamp
+                tablStamp.append(time.strftime("%H h %M min %S sec"))
                 # la reponse est stockee dans le tableau reponse
                 tablRep.append(rep)
         # sinon si le tableau qcm n'est pas vide
         elif tablQcm[i] != "" :
             # on affiche le contenu de l'index qcm
             print tablQcm[i]
-            # si i a atteint la longueur du tableau OU si l'index suivant contient une question
+            # si i a atteint la longueur du tableau OU si l'index suivant contient une question OU un theme
             if i == len(tablQuest) - 1 or tablQuest[i+1] != "" or tablTheme[i+1] != "":
                 # on demande la reponse au qcm
                 rep= raw_input("Ecrivez la bonne reponse. ")
+                # on stocke le timestamp dans le tableau Stamp
+                tablStamp.append(time.strftime("%H h %M min %S sec"))
                 # la reponse est stockee dans le tableau reponse
                 tablRep.append(rep)
 
 def write():
-    with open("index.html","w+") as f:
+    # on ouvre le fichier index.html en mode ecriture (ou on le cree si le fichier n'existe pas) la variable f correspond au fichier
+    with open("resultat/index.html","w+") as f:
         i = 0
         j = 0
+        # cette fonction ecrit le nom de la thematique dans les balises html appropriees
         def debTheme() : f.write('<article>\r\n<h3>' + tablTheme[i] + '</h3>\r\n')
-        def question() : f.write('<section>\r\n<h4><span>Question '+ str(j+1) +'</span> ' + tablQuest[i] + ' <span>' + time.strftime("%H h %M min %S sec") + '</span></h4>\r\n')
+        # cette fonction ecrit le numero de la question, nom de la question et timestamp dans leurs balises html appropriees
+        def question() : f.write('<section>\r\n<h4><span>Question '+ str(j+1) +'</span> ' + tablQuest[i] + ' <span>' + tablStamp[j] + '</span></h4>\r\n')
+        # cette fonction ecrit une reponse dans les balises html appropriees
         def reponse() : f.write('<p>' + tablRep[j] + '</p>\r\n</section>\r\n\n')
+        # cette fonction ferme la balise article qui a ete ouverte dans la fonction debTheme
         def finTheme() : f.write('</article>\r\n')
-        f.write('<html>\r\n<head>\r\n<meta http-equiv="content-type" content="text/html; charset=utf-8" />\r\n<title>evaluations du 03 janvier 2017 - Lunel</title>\r\n<link rel="stylesheet" type="text/css" href="style.css">\r\n</head>\r\n\n<body>\r\n<h1>evaluations du 03 janvier 2017 - Lunel</h1>\r\n<h2>Hicham El Yaagoubi</h2>\r\n')
+        # on ecrit le debut du document html (inutile d'en faire une fonction car on ne l'ecrit qu'une seule fois)
+        f.write('<html>\r\n<head>\r\n<meta http-equiv="content-type" content="text/html; charset=utf-8" />\r\n<title>evaluations du 03 janvier 2017 - Lunel</title>\r\n<link rel="stylesheet" type="text/css" href="style.css">\r\n</head>\r\n\n<body>\r\n<h1>evaluations du 03 janvier 2017 - Lunel</h1>\r\n<h2>Paul Maillard</h2>\r\n')
+        # tant que i se trouve dans l'etendue de la longueur du tableau
         for i in range(len(tablQuest)):
+            #  si le tableau theme n'est pas vide
             if tablTheme[i] != "":
+                # si on est pas au debut du questionnaire
                 if j != 0:
+                    # on ferme la balise article
                     finTheme()
+                # on ouvre un nouveau theme
                 debTheme()
+            # sinon si le tableau theme n'est pas vide
             elif tablQuest[i] != "":
+                # on appelle la fonction question
                 question()
+                # puis on appelle la fonction reponse
                 reponse()
-                if j < len(tablRep) - 1:
+                # si j est inferieur ou egal a la longueur du tableau reponse
+                if j <= len(tablRep):
+                    # on incremente j
                     j += 1
+        # on appelle la fonction finTheme
         finTheme()
+        # on ecrit les balises de fin du document
         f.write('</body>\r\n</html>')
 
 fetch()
